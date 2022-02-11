2b0e40b8886c53d876ae78da5eca2c551e623b81
Arlen002 <kurbanbaevarlen001@gmail.com>
2022-02-11 10:30:51 +0300

    tab1-16

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Circle {
     private static final int PI = 10;
 
-    static void area( ){
+    static void area(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Сан жазыныз: ");
-        int rs = sc.nextInt();
-        int aynt = PI * (rs * rs);
+        int radius = sc.nextInt();
+        int aynt = PI * (radius * radius);
         System.out.println("Айлананын аянты: "+aynt);
     }
     static void circumference(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Сан жазыныз: ");
-        int rs = sc.nextInt();
-        int around = PI * 2 * rs;
+        int radius = sc.nextInt();
+        int around = PI * 2 * radius;
         System.out.println("Айлананын узундугу: "+around);
 
     }
